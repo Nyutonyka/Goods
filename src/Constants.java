cba9af58bef51f641815f17cb00213d0dc78dc7a
nyuto <nuytonyka@gmail.com>
2022-04-29 18:05:50 +0300

    Add constants

---
+++ src/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+
+    public final static String DB_DRIVER = "jdbc:sqlite:";
+    public final static String DB_BASE_URL = "D:\\Hillel02\\Goods\\database\\";
+    public final static String DB_NAME = "Productdb.db";
+    public final static String TABLE_NAME = "Product";
+}
